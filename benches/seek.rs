bf65bc8cc8aed7821d3d76a212ab9eed51e01565
Peter Todd <pete@petertodd.org>
2025-03-23 20:09:38 +0000

    Add real-world seek benchmark

---
+++ benches/seek.rs
@@ -0,0 +1,47 @@
+#![feature(test)]
+#![feature(array_windows)]
+#![feature(array_chunks)]
+
+extern crate test;
+use test::bench::{Bencher, black_box};
+
+use std::io;
+
+use breccia::{Breccia, Search};
+
+#[bench]
+fn random_seeks(bencher: &mut Bencher) -> io::Result<()> {
+    let mut f = tempfile::tempfile()?;
+
+    let mut b = Breccia::create(f, ())?;
+
+    let mut offsets = vec![];
+    for i in 0u64 .. 1_000_000 {
+        let mut blob = vec![0u8; 0];
+        blob.extend_from_slice(&i.to_le_bytes());
+
+        blob.resize(8 + rand::random_range(0 .. 100), 42u8);
+
+        offsets.push((i, b.write_blob(&blob)?));
+    }
+
+    bencher.iter(|| {
+        for (i, expected_offset) in &offsets[1000..2000] {
+            assert_eq!(b.binary_search(|offset, blob| {
+                let blob = (&blob[0 .. 8]).try_into().unwrap();
+                let found = u64::from_le_bytes(blob);
+
+                if *i == found {
+                    Ok(Some(offset))
+                } else if *i < found {
+                    Err(Search::Left)
+                } else { // if i > found
+                    Err(Search::Right)
+                }
+            }).unwrap(),
+            Some(*expected_offset));
+        }
+    });
+
+    Ok(())
+}
